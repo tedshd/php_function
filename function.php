13c4bba906ab23dd88d3ae16a26c49151c1e02cf
Ted <tedshd@gmail.com>
2017-03-08 17:21:55 +0800

    Update function.php

--- function.php
+++ function.php
@@ -69,4 +69,60 @@ function httpPost($url,$params)
 //
 // refer -http://hayageek.com/php-curl-post-get/
 
+
+
+class QueryString {
+
+    public function query()
+    {
+        // handle query filter & rule
+        $filters = [
+            'q' => '',
+            'type' => '',
+            'start' => FILTER_VALIDATE_INT,
+            'rows' => FILTER_VALIDATE_INT,
+            'sort' => '',
+            'a' => [
+                'filter' => '',
+                'flags'  => FILTER_REQUIRE_ARRAY,
+            ],
+        ];
+
+        $query = filter_input_array(INPUT_GET, $filters);
+
+        foreach ($query as $key => $value) {
+            $query[$key] = (is_array($value)) ? array_map('trim', $value) : trim($value);
+        }
+        return $query;
+    }
+
+
+    public function urlGenerate($modified) {
+        foreach($modi)
+    }
+
+    public function urlGenerate($path, $new_query)
+    {
+        $query = $this -> query();
+        $query_string = http_build_query(array_filter($query));
+        if (!empty($query_string)) {
+            $query_string = '?' . $query_string;
+        }
+        if (!empty($new_query)) {
+            if (empty($query_string)) {
+                $new_query = '?' . $new_query;
+            } else {
+                $new_query = '&' . $new_query;
+            }
+        }
+        return $path . $query_string . $new_query;
+    }
+}
+
+// example
+// $QueryString = new QueryString;
+// var_dump($QueryString->query());
+// echo $QueryString -> urlGenerate();
+
+
 ?>
